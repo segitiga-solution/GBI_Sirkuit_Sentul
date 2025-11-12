063ef5a0dcc48c073bb8e4f02b8e86c7eb5fa48b
lesmanasatrya <khaistore99@gmail.com>
2025-11-12 13:54:32 +0700

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // <---- ini wajib agar asset relative, bukan absolut
-  build: {
-    outDir: "dist",
-  },
+  base: "/GBI_Sirkuit_Sentul/", // ⚠️ sesuai nama repo kamu di GitHub
 });
