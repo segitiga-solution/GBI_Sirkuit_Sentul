784e4a72f4a5f59f838bcaf21c25c2f854364a6f
lesmanasatrya <khaistore99@gmail.com>
2025-11-13 13:48:10 +0700

    pemberkatan pernikahan tambah 1

--- src/pages/PemberkatanPernikahan.tsx
+++ src/pages/PemberkatanPernikahan.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 export default function PelayananPemberkatanPernikahan() {
   const [currentImage, setCurrentImage] = useState(0);
 
-  const images = ["/pemberkatan2.jpeg"];
+  const images = ["/pemberkatan2.jpeg","/pemberkatan3.jpeg"];
 
   useEffect(() => {
     const interval = setInterval(() => {
